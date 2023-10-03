4a7448fadd4453f2100688100fc63d83b8b44a23
TANAY PANT <tanaypant71@gmail.com>
2023-10-03 21:07:39 +0530

    new

--- Arrays/findDuplicate.java
+++ Arrays/findDuplicate.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.* ;
+import java.util.*;
 
 import java.util.ArrayList;
 
@@ -15,5 +15,25 @@ public class Solution{
             seen.add(num);
         }
         return -1;
+    }
+}
+
+// Another way to solve it in O(n)
+
+import java.util.ArrayList;
+
+public class Solution {
+
+    public static int findDuplicate(ArrayList<Integer> arr) {
+
+        // Write your code here.
+        int n = arr.size();
+        int actualsum = 0;
+        int expectedsum = n * (n - 1) / 2;
+
+        for (int i = 0; i < n; i++) {
+            actualsum += arr.get(i);
+        }
+        return actualsum - expectedsum;
     }
 }
